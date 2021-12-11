2115652fb9126f8a636239ead12b442090d94ad3
Esa Salminen <esa_salminen@hotmail.com>
2021-12-11 11:47:18 +0200

    8.19 tehty, 1h

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -5,6 +5,7 @@ import { ALL_BOOKS } from '../queries'
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
   const [books, setBooks] = useState(null)
+  const [genreFilter,setGenreFilter] = useState('')
 
 
   useEffect(() => {
@@ -21,6 +22,10 @@ const Books = (props) => {
     return <div>...loading</div>
   }
 
+  const genres = books.flatMap(b => b.genres)
+  const filterGenreList = [...new Set(genres)]
+  
+  const filteredBooks = genreFilter.length > 0 ? books.filter(b => b.genres.includes(genreFilter)) : books
 
   return (
     <div>
@@ -37,7 +42,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -46,6 +51,10 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {filterGenreList.map(g =>
+        <button key={g} onClick={() => setGenreFilter(g)}>{g}</button>
+        )} <br/><br/>
+    <button onClick={() => setGenreFilter('')} >CLEAR FILTER</button>
     </div>
   )
 }
